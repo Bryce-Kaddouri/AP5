be821c18b88d28a576ad4c42cf439836fb20752a
Bryce-Kaddouri <bryce.kaddouri@gmail.com>
2022-09-22 12:38:32 +0400

    ajout gestion titre page visiiteur en fonction action

--- visiteur/js/script.js
+++ visiteur/js/script.js
@@ -56,22 +56,39 @@ $(document).ready(function () {
         })
     });
 
-
-
     // fonction qui permt de récupérer l'url de la page et de retourner un titre en fonction de l'url
     function getAction(url) {
-
         // recupération de l'action avec une expression régulière qui renvoi tous les parametre de l'url dans un tableau et on recupere le parametre action
         const uc = url.match(/uc=([^&]*)/)[1];
+        const action = url.match(/action=([^&]*)/)[1];
         // on retourne le titre en fonction de l'action
         if (uc == 'gererFrais') {
             return 'Saisie fiche de frais';
         } else if (uc == 'etatFrais') {
             return 'Consultation de mes fiches de frais';
+        } else if (uc == 'connexion') {
+            if (action == 'valideConnexion') {
+                return 'Accueil Visiteur'
+            } else if (action == 'deconnexion') {
+                return 'Authentification Visiteur'
+            }
+        } else {
+            return 'Authentification Visiteur'
+
         }
 
     }
+    // recuperation url page
     const url = window.location.href;
-    const titre = getAction(url);
+    const testParamUrl = url.split('?');
+    // explode url avec ? si taille > 1 ==> url a des prametres sinon pas de parametre
+    var titre = '';
+    if (testParamUrl.length > 1) {
+        titre = getAction(url);
+    } else {
+        titre = "Authentification Visiteur"
+    }
+    // affichge la variable titre dans le h1 de v_entete
     $('#titrePage').text(titre);
+
 })
