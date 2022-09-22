3d74bc26cb61766e8b071fed0b4261176d87839d
Bryce-Kaddouri <bryce.kaddouri@gmail.com>
2022-09-22 14:17:09 +0400

    titre compta

--- comptable/js/script.js
+++ comptable/js/script.js
@@ -100,6 +100,42 @@ $(document).ready(function () {
             }
         })
     });
+
+    // fonction qui permt de récupérer l'url de la page et de retourner un titre en fonction de l'url
+    function getAction(url) {
+        // recupération de l'action avec une expression régulière qui renvoi tous les parametre de l'url dans un tableau et on recupere le parametre action
+        const uc = url.match(/uc=([^&]*)/)[1];
+        const action = url.match(/action=([^&]*)/)[1];
+        // on retourne le titre en fonction de l'action
+        if (uc == 'validerFrais') {
+            return 'Validation fiche de frais';
+        } else if (uc == 'suivrePaiement') {
+            return 'Suivi paiement fiches de frais';
+        } else if (uc == 'connexion') {
+            if (action == 'valideConnexion') {
+                return 'Accueil Comptable'
+            } else if (action == 'deconnexion') {
+                return 'Authentification Comptable'
+            }
+        } else {
+            return 'Authentification Visiteur'
+
+        }
+
+    }
+    // recuperation url page
+    const url = window.location.href;
+    const testParamUrl = url.split('?');
+    // explode url avec ? si taille > 1 ==> url a des prametres sinon pas de parametre
+    var titre = '';
+    if (testParamUrl.length > 1) {
+        titre = getAction(url);
+    } else {
+        titre = "Authentification Visiteur"
+    }
+    // affichge la variable titre dans le h1 de v_entete
+    $('#titrePage').text(titre);
+
 });
 
 
